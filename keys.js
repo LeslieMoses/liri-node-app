d7c3c6369ea9cf94649218e0abc433ea5e5b3f79
LeslieMoses <lesmoses@gmail.com>
2017-05-31 15:12:12 -0400

    Update keys.js

--- keys.js
+++ keys.js
@@ -3,8 +3,8 @@ console.log('this is loaded');
 // 
   exports.twitterKeys = {
 
-  consumer_key: "VfW7sHZIZprrq7yfDWDsD6SZi",
-  consumer_secret: "mdVkAV3YfzyAWgsySVcxhzpxYK9kIuwFKgoduyIj4ovJXRkItL",
-  access_token_key: "2273609821-2DOGOcGXfHGFeT7O2aSoul7K7Y1w2OEeKqhxV3q",
-  access_token_secret: "U2ZSHanmnxA8cyuml4XNBX5UxitpZuP6kNKpZ8ztWFBL1",
+  consumer_key: "i",
+  consumer_secret: "v",
+  access_token_key: "v",
+  access_token_secret: "v",
 }
